3424bc06d42e6221857c44bd37d38c0060eb0401
tahataheriy <tahataheri330@gmail.com>
2023-10-13 16:54:28 +0330

    f2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+def create100(requests):
+    me = User.objects.get(username='admin')
+    for i in range(100):
+        p=Post.objects.create(author=me, title=f'fotball omid{i}', text=f'fotball omid dar brabar hongkong baxt{i}')
+#    p=post()
+#    p.title='fotball omid'
+#    p.text='fotball omid dar barabar hongkong baxt'
+#    p.author=
+        p.publish()
+    return HttpResponse("Done")
